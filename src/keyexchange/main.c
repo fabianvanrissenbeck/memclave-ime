cedc6800aa83ac71da5e7038b3e0a0edf8809792
fabianvanrissenbeck <fabian.van-rissenbeck@cispa.de>
2025-08-24 08:02:13 +0200

    implemented second key exchange stage

--- src/keyexchange/main.c
+++ src/keyexchange/main.c
@@ -1,4 +1,5 @@
 #include "mbedtls/dhm.h"
+#include "mbedtls/bignum.h"
 
 #include <alloc.h>
 #include <stdio.h>
@@ -6,54 +7,109 @@
 #include <string.h>
 #include <perfcounter.h>
 
-static mbedtls_dhm_context ctx;
+extern uint32_t __mram_noinit __ime_debug_out[16];
 
-const uint8_t ime_dhm_prime[] = MBEDTLS_DHM_RFC3526_MODP_2048_P_BIN;
+// little endian version of MBEDTLS_DHM_RFC3526_MODP_2048_P_BIN
+const uint32_t ime_raw_prime[] = {
+    0xffffffff, 0xffffffff, 0x8aacaa68, 0x15728e5a,
+    0x98fa0510, 0x15d22618, 0xea956ae5, 0x3995497c,
+    0x95581718, 0xde2bcbf6, 0x6f4c52c9, 0xb5c55df0,
+    0xec07a28f, 0x9b2783a2, 0x180e8603, 0xe39e772c,
+    0x2e36ce3b, 0x32905e46, 0xca18217c, 0xf1746c08,
+    0x4abc9804, 0x670c354e, 0x7096966d, 0x9ed52907,
+    0x208552bb, 0x1c62f356, 0xdca3ad96, 0x83655d23,
+    0xfd24cf5f, 0x69163fa8, 0x1c55d39a, 0x98da4836,
+    0xa163bf05, 0xc2007cb8, 0xece45b3d, 0x49286651,
+    0x7c4b1fe6, 0xae9f2411, 0x5a899fa5, 0xee386bfb,
+    0xf406b7ed, 0xbff5cb6, 0xa637ed6b, 0xf44c42e9,
+    0x625e7ec6, 0xe485b576, 0x6d51c245, 0x4fe1356d,
+    0xf25f1437, 0x302b0a6d, 0xcd3a431b, 0xef9519b3,
+    0x8e3404dd, 0x514a0879, 0x3b139b22, 0x20bbea6,
+    0x8a67cc74, 0x29024e08, 0x80dc1cd1, 0xc4c6628b,
+    0x2168c234, 0xc90fdaa2, 0xffffffff, 0xffffffff
+};
+
+const uint32_t ime_raw_sk[] = {
+    0x69c7e189, 0xd0997133, 0xf1623124, 0xc7169218,
+    0xbad0d374, 0x6b8e7824, 0x83338e41, 0x00db6a03,
+};
+
+const mbedtls_mpi ime_dhm_p = {
+    .private_p = (uint32_t*) ime_raw_prime,
+    .private_n = sizeof(ime_raw_prime) / sizeof(uint32_t),
+    .private_s = 1
+};
+
+const mbedtls_mpi ime_dhm_s = {
+    .private_p = (uint32_t*) ime_raw_sk,
+    .private_n = sizeof(ime_raw_sk) / sizeof(uint32_t),
+    .private_s = 1
+};
+
+__attribute__((aligned(4)))
 const uint8_t ime_dhm_gen[] = MBEDTLS_DHM_RFC3526_MODP_2048_G_BIN;
-const uint8_t ime_secret_key[32] = { 0 };
 
-static struct {
-    size_t len;
-    uint64_t state[2048]; // 16 KiB
-} g_alloc_state;
+size_t n_alloc = 0;
+size_t m_alloc = 0;
 
-void mbedtls_zeroize_and_free(void* buf, size_t len) {}
+struct ime_xchg_io {
+    union {
+        struct {
+            uint32_t client_pk_in[64];
+            uint32_t xchg_cnt_in[4];
+        } in;
+        struct {
+            uint32_t xchg_shared_out[64];
+            uint32_t xchg_cnt_out[4];
+        } out;
+    };
+};
 
-void mbedtls_platform_zeroize(void* buf, size_t len) {}
+__attribute__((section(".data.persist")))
+struct ime_xchg_io g_xchg_io;
+
+const mbedtls_mpi ime_client_pub = {
+    .private_p = g_xchg_io.in.client_pk_in,
+    .private_n = sizeof(g_xchg_io.in.client_pk_in) / sizeof(uint32_t),
+    .private_s = 1
+};
 
 void* calloc(size_t n, size_t sz) {
-    size_t offset = (sz + 7) / 8;
-    void* res = &g_alloc_state.state[g_alloc_state.len];
-
-    if (g_alloc_state.len + offset < 2048) {
-        g_alloc_state.len += offset;
-        return res;
-    } else {
-        return NULL;
-    }
+    // alloc_db[n_alloc++] = n * sz;
+    n_alloc += 1;
+    m_alloc += n * sz;
+    return buddy_alloc(n * sz);
 }
 
-int rng(void* ctx, uint8_t* buf, size_t len) { return 0; }
-
-static uint8_t output[512] = { 0 };
+void free(void* p) {
+    buddy_free(p);
+}
 
 int main(void) {
-    mbedtls_mpi p;
-    mbedtls_mpi g;
-    mbedtls_mpi X;
-    mbedtls_mpi x;
+    mbedtls_mpi pk;
 
-    mbedtls_mpi_init(&p);
-    mbedtls_mpi_init(&g);
-    mbedtls_mpi_init(&X);
-    mbedtls_mpi_init(&x);
+    buddy_init(8192 * 2);
+    mbedtls_mpi_init(&pk);
 
-    assert(mbedtls_mpi_read_binary(&p, ime_dhm_prime, sizeof(ime_dhm_prime)) == 0);
-    assert(mbedtls_mpi_read_binary(&g, ime_dhm_gen, sizeof(ime_dhm_gen)) == 0);
-    assert(mbedtls_mpi_read_binary(&x, ime_secret_key, sizeof(ime_secret_key)) == 0);
+    // overwrite the client key with 2 for testing purposes. TODO: Remove this
 
-    perfcounter_config(COUNT_CYCLES, true);
-    assert(mbedtls_mpi_exp_mod(&X, &g, &x, &p, NULL) == 0);
+    for (int i = 1; i < 64; ++i) {
+        g_xchg_io.in.client_pk_in[i] = 0;
+    }
 
-    printf("mbedtls_mpi_exp_mode(): %llums\n", (uint64_t) perfcounter_get() * 1000 / CLOCKS_PER_SEC);
+    g_xchg_io.in.client_pk_in[0] = 2;
+
+    int res = mbedtls_mpi_exp_mod(&pk, &ime_client_pub, &ime_dhm_s, &ime_dhm_p, NULL);
+
+    if (res == MBEDTLS_ERR_MPI_ALLOC_FAILED) {
+        printf("Allocation Failed\n");
+    }
+
+    assert(res == 0);
+
+    __ime_debug_out[0] = pk.private_p[0];
+
+    // TODO: Hand over shared secret to third key exchange stage
+    asm("stop");
+    return 0;
 }
