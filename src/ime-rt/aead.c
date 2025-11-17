f7d3953c904f0bd6437541abad57c1bd88b67760
fabianvanrissenbeck <fabian.van-rissenbeck@cispa.de>
2025-11-17 13:57:51 +0100

    factored out common operations in aead

--- src/ime-rt/aead.c
+++ src/ime-rt/aead.c
@@ -2,6 +2,35 @@
 #include "poly.h"
 #include "ime.h"
 
+static void ime_chacha_crypt(const uint32_t* key, const uint32_t* iv,
+                             size_t len, const uint32_t* buf, uint32_t* out_buf)
+{
+    uint32_t block[16];
+
+    for (size_t i = 0; i < (len + 63) / 64; ++i) {
+        __ime_chacha_blk(key, i + 1, iv[0], iv[1], iv[2], block);
+
+        for (size_t j = 0; j < 16 && i * 16 + j < len / 4; ++j) {
+            out_buf[i * 16 + j] = buf[i * 16 + j] ^ block[j];
+        }
+    }
+}
+
+static void ime_poly_simple(const uint32_t* key, const uint32_t* iv, size_t len, const uint32_t* buf, uint32_t* tag) {
+    uint32_t block[16];
+    poly_context ctx;
+
+    __ime_chacha_blk(key, 0, iv[0], iv[1], iv[2], block);
+    poly_init(&ctx, block);
+
+    for (size_t i = 0; i < len / 16; ++i) {
+        poly_feed_block(&ctx, &buf[i * 4]);
+    }
+
+    poly_feed_block(&ctx, (uint32_t[4]) { 0, 0, len, 0 });
+    poly_finalize(&ctx, tag);
+}
+
 void ime_aead_enc(const uint32_t* key, const uint32_t* p_iv,
                   size_t len, const uint32_t* buf, uint32_t* out_buf,
                   uint32_t* out_tag, uint32_t* out_iv)
@@ -16,26 +45,8 @@ void ime_aead_enc(const uint32_t* key, const uint32_t* p_iv,
         iv = iv_buf;
     }
 
-    poly_context ctx;
-    uint32_t block[16];
-
-    __ime_chacha_blk(key, 0, iv[0], iv[1], iv[2], block);
-    poly_init(&ctx, block);
-
-    for (size_t i = 0; i < (len + 63) / 64; ++i) {
-        __ime_chacha_blk(key, i + 1, iv[0], iv[1], iv[2], block);
-
-        for (size_t j = 0; j < 16 && i * 16 + j < len / 4; ++j) {
-            out_buf[i * 16 + j] = buf[i * 16 + j] ^ block[j];
-        }
-    }
-
-    for (size_t i = 0; i < len / 16; ++i) {
-        poly_feed_block(&ctx, &out_buf[i * 4]);
-    }
-
-    poly_feed_block(&ctx, (uint32_t[4]) { 0, 0, len, 0 });
-    poly_finalize(&ctx, out_tag);
+    ime_chacha_crypt(key, iv, len, buf, out_buf);
+    ime_poly_simple(key, iv, len, out_buf, out_tag);
 
     if (out_iv) {
         out_iv[0] = iv[0];
@@ -47,33 +58,17 @@ void ime_aead_enc(const uint32_t* key, const uint32_t* p_iv,
 bool ime_aead_dec(const uint32_t* key, const uint32_t* iv, const uint32_t* tag,
                   size_t len, const uint32_t* buf, uint32_t* out_buf)
 {
-    poly_context ctx;
-    uint32_t block[16];
     bool valid = true;
+    uint32_t computed_tag[4];
 
-    __ime_chacha_blk(key, 0, iv[0], iv[1], iv[2], block);
-    poly_init(&ctx, block);
-
-    for (size_t i = 0; i < len / 16; ++i) {
-        poly_feed_block(&ctx, &buf[i * 4]);
-    }
-
-    poly_feed_block(&ctx, (uint32_t[4]){ 0, 0, len, 0 });
-    poly_finalize(&ctx, block);
+    ime_poly_simple(key, iv, len, buf, computed_tag);
 
     for (size_t i = 0; i < 4; ++i) {
-        valid &= block[i] == tag[i];
+        valid &= computed_tag[i] == tag[i];
     }
 
     if (!valid) { return false; }
 
-    for (size_t i = 0; i < (len + 63) / 64; ++i) {
-        __ime_chacha_blk(key, i + 1, iv[0], iv[1], iv[2], block);
-
-        for (size_t j = 0; j < 16 && i * 16 + j < len / 4; ++j) {
-            out_buf[i * 16 + j] = buf[i * 16 + j] ^ block[j];
-        }
-    }
-
+    ime_chacha_crypt(key, iv, len, buf, out_buf);
     return true;
 }
