c52c2d036718afc0b191818679611b6ac16c4cc4
fabianvanrissenbeck <fabian.vanrissenbeck@gmail.com>
2025-12-09 20:14:57 +0100

    bug in ime-rt: NR_TASKLETS in the finishing barrier was one instead of actual linker value

--- debug.sh
+++ debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --mount type=bind,src=./,dst=/project,ro --workdir /project -it fabianvanrissenbeck/upmem:ubuntu dpu-lldb "$@"
+docker run --mount type=bind,src=./,dst=/project,ro --workdir /project -it ghcr.io/deinernstjetzt/upmem:latest dpu-lldb "$@"
--- src/ime-rt/ime.c
+++ src/ime-rt/ime.c
@@ -4,21 +4,21 @@
 #include <mutex.h>
 
 MUTEX_INIT(__ime_tasklet_lock);
-static volatile uint32_t __ime_tasklet_count = NR_TASKLETS;
+extern volatile uint32_t __ime_tasklet_count;
 
 __attribute__((section(".data.persist.last"))) ime_load_params g_load_prop = { 0 };
 
 void __ime_stop_tasklet(void) {
     if (me() == 0) {
-        uint32_t count = NR_TASKLETS;
+        volatile uint32_t count;
 
-        while (count != 1) {
+        do {
             mutex_lock(__ime_tasklet_lock);
             count = __ime_tasklet_count;
             mutex_unlock(__ime_tasklet_lock);
 
             for (volatile int i = 0; i < 1000; ++i) {}
-        }
+        } while (count != 1);
     } else {
         mutex_lock(__ime_tasklet_lock);
         __ime_tasklet_count -= 1;
