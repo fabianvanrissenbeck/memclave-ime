40e6c382a2dc2993d9cb1ab129bb198cd1611e4c
fabianvanrissenbeck <fabian.vanrissenbeck@gmail.com>
2026-01-15 17:49:49 +0100

    reverted to old add subkernel

--- src/add/main.c
+++ src/add/main.c
@@ -1,20 +1,12 @@
-#define VEC_SIZE 64
-
 #include <mram.h>
-#include "mutex.h"
-
-#define UNDEFINED_VAL (-1)
-volatile int shared_variable = UNDEFINED_VAL;
-MUTEX_INIT(my_mutex);
+#define VEC_SIZE 64
 
 __mram uint64_t a[VEC_SIZE];
 __mram uint64_t b[VEC_SIZE];
 __mram uint64_t c[VEC_SIZE];
 
 int main() {
-  mutex_lock(my_mutex);
     for (int i = 0; i < VEC_SIZE; i++) {
         c[i] = a[i] + b[i];
     }
-  mutex_unlock(my_mutex);
 }
